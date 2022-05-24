b055862dacddfe4f3baf3dff5762011537bb905c
Алексей Кудрявцев <alekudryavtsev@gmail.com>
2022-05-24 15:03:08 +0300

    My first commit

--- program.py
+++ program.py
@@ -1,1 +1,12 @@
-print('Я домашка')
+from typing import List, Union
+
+
+def series_sum(incoming: List[Union[int, float]]) -> str:
+    """Принимает на вход список из строк и чисел с плавающей точкой, приводит его элементы к строкам и конкатенирует их.
+    """
+    result = ''
+    for i in incoming:
+        result += str(i)
+    return result
+
+series_sum([1, 1.5])
